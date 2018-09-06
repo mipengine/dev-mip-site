ef85a14b18f8af43f51b01f61d33a1af3d7554e5
wangyisheng <wangyisheng@baidu.com>
2018-09-06 11:19:59 +0800

    完善注释

--- actions/weather.js
+++ actions/weather.js
@@ -27,7 +27,8 @@ module.exports = {
     // 当访问 URL 是 '/weather/shanghai?type=text' 时
     // `ctx.params` 等于 `{city: 'shanghai'}`
     // `ctx.query` 等于 `{type: 'text'}`
-    console.log(ctx.params, ctx.query)
+    console.log('ctx.params =', ctx.params)
+    console.log('ctx.query =', ctx.query)
 
     // 发送请求获取远程数据
     let {body} = await got('https://query.yahooapis.com/v1/public/yql?q=' +
@@ -35,9 +36,9 @@ module.exports = {
       '&format=json', {json: true})
     let weatherInfo = body.query.results.channel.item.condition
 
-    // ctx.body = render(templatePath, data)
+    // 使用 ctx.body = render(templatePath, data) 设置响应
     // 其中 templatePath 相对于根目录，因此应该是 templates/weather.tpl 而不是 ../templates/weather.tpl
-    // 可以省略 templates/ 和后缀名，因此简写为 weather
+    // 为了简便，可以省略 templates/ 和后缀名(.tpl)，所以最终简写为 weather
     ctx.body = render('weather', {
       date: weatherInfo.date,
       temp: weatherInfo.temp,
