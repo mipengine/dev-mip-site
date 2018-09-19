f449c52447890a40a212bb189196f06e8df136a8
liuruoran88 <liuruoran88@163.com>
2018-09-19 17:06:24 +0800

    upload修改

--- upload.sh
+++ upload.sh
@@ -49,6 +49,23 @@ cp -rf . ../$output/
 cd ../$output/
 rm 'upload.sh'
 rm -rf '.git'
+
+#修改组件模板变量的三个文件
+newContent='所需脚本| {% if: {{ name }} %}{% if: {{ name }} === 'mip2-extensions' %}[https://c.mipcdn.com/static/v2/{{ compName }}/{{ compName }}.js](https://c.mipcdn.com/static/v2/{{ compName }}/{{ compName }}.js){% else %}[https://c.mipcdn.com/extensions/platform/v2/{{name}}/{{ compName }}/{{ compName }}.js](https://c.mipcdn.com/extensions/platform/v2/{{name}}/{{ compName }}/{{ compName }}.js){% /if %}{% /if %}'
+
+#1.mip-example.html
+sed -i '' 's/mip-example/\{\{ compName \}\}/g' mip-component/components/mip-example/example/mip-example.html
+#2.README.md
+sed -i '' 's/mip-example/\{\{ compName \}\}/' mip-component/components/mip-example/README.md
+sed -i '' "/所需脚本/ c\ 
+${newContent}
+" mip-component/components/mip-example/README.md
+#3.package.json
+sed -i '' 's/\"name\": \"name\"/\"name\": \"\{\{ name \}\}\"/g' package.json
+sed -i '' 's/\"description\": \"description\"/\"description\": \"\{\{ description \}\}\"/g' package.json
+sed -i '' 's/\"author\": \"yourname\"/\"author\": \"\{\{ yourname \}\}\"/g' package.json
+
+
 zip -r ../mip-plugin-site-template.zip . .[^.]*
 rm -rf ./* 
 mv ../mip-plugin-site-template.zip ./mip-plugin-site-template.zip
